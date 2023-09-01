a49f6f787709d1abc47a8c030f4f7902af6a3fcf
Mariga123 <johnmariga8@gmail.com>
2023-09-01 22:59:37 +0300

    created models for view and installed app services

--- techsolutions/urls.py
+++ techsolutions/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("services/", include("services.urls")),
     path('admin/', admin.site.urls),
 ]
