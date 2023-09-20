cd67c929901766fe86efabbab0074d29cf4bf62e
Mariga123 <johnmariga8@gmail.com>
2023-09-20 19:09:06 +0300

    created a contact response form in django

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
+from .views import contact, success
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("service", views.service, name="services"),
+    path('contact/', contact, name='contact'),
+    path('success/', success, name='success'),
    
 ]
--- services/views.py
+++ services/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
+from techsolutions.forms import ContactForm
 
 def index(request): 
     return render(request, 'index.html')
 
 def service(request): 
     return render(request, 'services.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            pass
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'index.html')
